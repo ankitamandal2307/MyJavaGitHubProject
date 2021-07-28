7aecbaea9de2172d56fa90e9581d11ca8493f6ca
AnkitaMandal <2017.ankita.mandal@ves.ac.in>
2021-07-28 13:03:01 +0530

    initial commit

---
+++ MyJavaGitHubProject/src/main/java/com/lti/gitdemo/UserApp.java
@@ -0,0 +1,10 @@
+package com.lti.gitdemo;
+
+public class UserApp {
+	public static void main(String[] args) {
+		
+		Person p = new Person(100, "Ankita", "Mandal");
+		System.out.println(p);
+	
+	}
+}
